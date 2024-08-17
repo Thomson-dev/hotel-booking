74ffedb36d47e366c87802d3a980d74d97dbcac9
sherifsani <sanisherif11@gmail.com>
2024-08-17 20:38:14 +0100

    modified the services

---
+++ data/index.ts
@@ -0,0 +1,27 @@
+const services = [
+  {
+    service: "Pick Up & Drop",
+    description:
+      "Orci varius natoque penatibus magnis ders disney parturient ridiculus",
+  },
+  {
+    service: "Fibre Internet",
+    description:
+      "Orci varius natoque penatibus magnis ders disney parturient ridiculus",
+  },
+  {
+    service: "Breakfast",
+    description:
+      "Orci varius natoque penatibus magnis ders disney parturient ridiculus",
+  },
+  {
+    service: "Swimming Pool",
+    description:
+      "Orci varius natoque penatibus magnis ders disney parturient ridiculus",
+  },
+  {
+    service: "Parking Space",
+    description:
+      "Orci varius natoque penatibus magnis ders disney parturient ridiculus",
+  },
+];
